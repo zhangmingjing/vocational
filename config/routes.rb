b44e3f51a9beaf24b58e2692dc2f58cd74434a17
zhangw <809405715@qq.com>
2015-04-10 22:14:09 +0800

    4-10 22:15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'video/index'
 
+  get 'welcome/index'
+  get 'video/nowplay'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
